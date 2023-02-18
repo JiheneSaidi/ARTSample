c23c73eb979f15aa97eb5113fb1e3a6202d3224f
Jihene Saidi <JSaidi@se.linedata.com>
2023-02-18 01:03:31 +0100

    ART Sample

---
+++ cypress/e2e/UsersForm.cy.js
@@ -0,0 +1,41 @@
+describe('Check user Form', () => {
+
+  beforeEach(() => {
+      cy.visit('https://testpages.herokuapp.com/styled/index.html');
+      cy.get("#htmlformtest").click();
+
+  });
+
+  it('From Page is displayed', () => {
+
+      cy.get("h1").then(($title) => {
+          expect($title).to.have.text("Basic HTML Form Example");
+      });
+
+      cy.get("#HTMLFormElements").should('be.visible');
+
+  });
+
+  it('Submit correct form', () => {
+
+        var username = "Jihene";
+        var password = "Pass1";
+
+        cy.get('input[name="username"]').type(username);
+        cy.get('input[name="password"]').type(password);
+        cy.get('input[value="cb1"]').check();
+        cy.get('input[value="cb1"]').should('be.checked')
+        cy.get('input[value="rd1"]').click();
+        cy.get('select[name="dropdown"]').select("dd2");
+        cy.get('input[Value="submit"]').click();
+
+        cy.get("h2").then(($title) => {
+            expect($title).to.have.text("Submitted Values");
+        });
+
+        cy.get("#_valueusername").then(($usernameresult) => {
+            expect($usernameresult).to.have.text(username);
+        });
+   })
+
+})
